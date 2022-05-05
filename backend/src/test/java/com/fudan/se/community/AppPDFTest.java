267b92caa65558bbfcfc8f9f87f3d46ca8d0bdc4
YueSun <10058248+yuesuncatchup@user.noreply.gitee.com>
2022-05-05 23:10:33 +0800

    configuration for cloud

--- backend/src/test/java/com/fudan/se/community/AppPDFTest.java
+++ backend/src/test/java/com/fudan/se/community/AppPDFTest.java
@@ -22,21 +22,21 @@ import java.nio.file.Paths;
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 public class AppPDFTest {
-    @Test
-    void generateMarkdownDocsToFile() throws Exception {
-        //    输出Markdown到单文件
-        Swagger2MarkupConfig config = new Swagger2MarkupConfigBuilder()
-                .withMarkupLanguage(MarkupLanguage.MARKDOWN)
-                .withOutputLanguage(Language.ZH)
-                .withPathsGroupedBy(GroupBy.TAGS)
-                .withGeneratedExamples()
-                .withoutInlineSchema()
-                .build();
-
-        Swagger2MarkupConverter.from(new URL("http://localhost:8081/v2/api-docs?group=adweb"))
-                .withConfig(config)
-                .build()
-                .toFile(Paths.get("src/main/resources/docs/markdown"));
-    }
+//    @Test
+//    void generateMarkdownDocsToFile() throws Exception {
+//        //    输出Markdown到单文件
+//        Swagger2MarkupConfig config = new Swagger2MarkupConfigBuilder()
+//                .withMarkupLanguage(MarkupLanguage.MARKDOWN)
+//                .withOutputLanguage(Language.ZH)
+//                .withPathsGroupedBy(GroupBy.TAGS)
+//                .withGeneratedExamples()
+//                .withoutInlineSchema()
+//                .build();
+//
+//        Swagger2MarkupConverter.from(new URL("http://localhost:8081/v2/api-docs?group=adweb"))
+//                .withConfig(config)
+//                .build()
+//                .toFile(Paths.get("src/main/resources/docs/markdown"));
+//    }
 }
 
